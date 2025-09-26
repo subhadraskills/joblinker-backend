cde04a79cf664240078aa63d7c6d660342155b6e
Subhadra Kumari <subhadraskills@gmail.com>
2025-09-26 09:06:49 +0530

    update

--- src/app.ts
+++ src/app.ts
@@ -7,14 +7,13 @@ const app = express();
     
 app.use(cors({
   origin:[
-     "https://joblinker-frontend.vercel.app",    
+     "https://joblinker-frontend.vercel.app",       
     "http://localhost:5173", 
-  ],
-  credentials: true
+  ]
 }));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));    
-
+   
 
 app.use("/api/auth", authRoutes);
 app.use("/api/jobs", jobRoutes);
